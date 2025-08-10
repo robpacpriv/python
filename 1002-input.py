e511976e0828f6346b869a74f99d6acb4d64f6f1
Robert Pacula <P0566RP@msc.corpdir.net>
2025-08-10 15:12:26 +0200

    two games: multiplication and division

--- 1002-input.py
+++ 1002-input.py
@@ -1,3 +1,8 @@
-name = input("Enter your name:")
+# run py code: CTRL + R
+# format document by pep8: SHIFT + ALT + F
 
-# print ("You are", name)
+name = input("Please provide your name: ")
+country = input("Please provide your country: ")
+
+print ("Your name is", name)
+print ("You country is", country)
---
+++ 1003-game-multiplication-to-100.py
@@ -0,0 +1,38 @@
+# run py code: CTRL + R
+# format document by pep8: SHIFT + ALT + F
+
+import random
+
+#name = input("Please provide your name: ")
+goodAnswers = 0
+numberOfQuestions = 10
+
+for x in range(numberOfQuestions):
+
+    resolut = None
+
+    number1 = random.randint(2, 10)
+    number2 = random.randint(2, 10)
+
+    question = ("Please provide resoult multiplication of ") + str(number1) + (" and ") + str(number2) + (": ")
+
+    while resolut is None: 
+        try:
+            resolut = int(input(question))
+        except ValueError:
+            continue
+
+    if resolut == (number1*number2):
+        goodAnswers = goodAnswers + 1
+        print ("\x1b[6;30;42m" + "Good, you are erning star: *" + "\x1b[0m")
+    else:
+        print ("\x1b[6;30;41m" + "Oh no, you made a mistake :-(" + "\x1b[0m")
+
+if goodAnswers == numberOfQuestions:
+    print ("Great job! You are the best! You can eat icecreams now ;-) You have earned:", goodAnswers, "of", numberOfQuestions, "stars")
+if goodAnswers < numberOfQuestions and goodAnswers >= numberOfQuestions/2:
+    print ("Not bad but also not perfect. Keep trying ;-) You have earned:", goodAnswers, "of", numberOfQuestions, "stars")
+if goodAnswers > 0 and goodAnswers < numberOfQuestions/2:
+    print ("Keep learning and try again. You have earned:", goodAnswers, "of", numberOfQuestions, "stars")
+if goodAnswers == 0:
+    print ("Better go back to learn. YYou have earned:", goodAnswers, "of", numberOfQuestions, "stars")
