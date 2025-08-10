7d41542d2e36b6e4defe90f0cc9b90da15fc769a
Robert Pacula <P0566RP@msc.corpdir.net>
2025-08-10 12:16:48 +0200

    printy

---
+++ 1001-print_variable.py
@@ -0,0 +1,41 @@
+# run py code: CTRL + R
+# format document by pep8: SHIFT + ALT + F
+
+print("\n----------------------------------\n")
+
+# variable string type
+name = ("\"Robik\"")
+year = 43
+year = str(year)
+
+print("Hello", name)
+print("Are you", year, "years old?")
+print("It looks that", name, "is going learn Python ;-)")
+print("That's great idea!")
+
+print("\n----------------------------------\n")
+
+# concate two string into variable string
+fullprint = ("Hi again ") + name + ("Are you ") + year + (" years old?")
+
+# show variable
+print(fullprint)
+
+print("\n----------------------------------\n")
+
+# Using tripple hash
+print("You Learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+
+print("""\nYou Learning Path:
+\t- Python Basics
+\t- Data Engineering
+\t- AI""")
+
+print("\n----------------------------------\n")
+# Challange Course 5:
+
+domain = ("pacula.eu")
+
+print("datawithrobik@", domain, sep="")
+print("support@", domain, sep="")
+print("www.", domain, sep="")
---
+++ 1002-input.py
@@ -0,0 +1,3 @@
+name = input("Enter your name:")
+
+# print ("You are", name)
